85d133aea5e4bf4f43203a018fa108cb2a889e61
gabriel <gabrielaguirre921@gmail.com>
2022-04-21 13:19:07 -0500

    fixing issue: pixelated block still remains when no person is on video.

--- android/src/main/java/com/opentokreactnative/mlkit/graphics/FaceOverlay.java
+++ android/src/main/java/com/opentokreactnative/mlkit/graphics/FaceOverlay.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 
 import com.google.mlkit.vision.face.Face;
 import com.opentokreactnative.mlkit.utils.BitmapEffects;
@@ -31,7 +32,7 @@ public class FaceOverlay {
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.FILL);
 
-        drawHeadBoxPlaceholder(originalBitmap, tempCanvas, paint, face);
+        drawHeadPlaceholderCircle(face, tempCanvas, paint);
 
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         drawPixelatedFace(originalBitmap, tempCanvas, paint, face);
@@ -39,38 +40,28 @@ public class FaceOverlay {
         canvas.drawBitmap(output, 0, 0, null);
     }
 
-    private void drawHeadBoxPlaceholder(Bitmap original, Canvas canvas, Paint paint, Face face) {
-        int x = face.getBoundingBox().centerX();
-        int y = face.getBoundingBox().centerY();
+    private void drawHeadPlaceholderCircle(Face face, Canvas canvas, Paint paint) {
+        Rect boundingBox = face.getBoundingBox();
+        int x = boundingBox.centerX();
+        int y = boundingBox.centerY();
+        float radius = boundingBox.height() / 1.4f;
 
-        int left = x - (face.getBoundingBox().width() / 2);
-        int top = y - (face.getBoundingBox().height() / 2) ;
-        int right = x + (face.getBoundingBox().width() / 2);
-        int bottom = y + (face.getBoundingBox().height() / 2);
-
-        if (left < 0) {
-            left = 0;
-        }
-        if (top < 0) {
-            top = 0;
-        }
-        if (right > original.getWidth()) {
-            right = original.getWidth();
-        }
-        if (bottom > original.getHeight()) {
-            bottom = original.getHeight();
-        }
-        canvas.drawRect(left, top, right, bottom, paint);
+        canvas.drawCircle(x, y, radius, paint);
     }
 
     private void drawPixelatedFace(Bitmap original, Canvas canvas, Paint paint, Face face) {
-        int x = face.getBoundingBox().centerX();
-        int y = face.getBoundingBox().centerY();
+        Rect boundingBox = face.getBoundingBox();
+        int halfWidth = boundingBox.width() / 2;
+        int halfHeight = boundingBox.height() / 2;
 
-        int left = x - (face.getBoundingBox().width() / 2);
-        int top = y - (face.getBoundingBox().height() / 2);
-        int right = x + (face.getBoundingBox().width() / 2);
-        int bottom = y + (face.getBoundingBox().height() / 2);
+        int x = boundingBox.centerX();
+        int y = boundingBox.centerY();
+
+        int headOffset = 25;
+        int left = x - halfWidth - headOffset;
+        int top = y - halfHeight - headOffset;
+        int right = x + halfWidth + headOffset;
+        int bottom = y + halfHeight + headOffset;
 
         if (left < 0) {
             left = 0;
